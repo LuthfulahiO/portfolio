675794fba386ffdee45e7be8e6a941f075995264
Luthfulahi <luteeph@gmail.com>
2019-10-31 10:47:15 +0100

    Initial commit from gatsby: (https://github.com/EmaSuriano/gatsby-starter-mate.git)

---
+++ src/components/SocialLink.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Link } from 'rebass';
+import Tippy from '@tippy.js/react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import FontAwesomeIcon from 'react-fontawesome';
+
+const IconLink = styled(Link)`
+  transition: color 0.5s;
+  color: ${props =>
+    props.theme.colors[props.color] || props.theme.colors.primary};
+  text-decoration: none;
+
+  &:hover {
+    color: ${props => props.theme.colors.primaryLight};
+  }
+`;
+
+const SocialLink = ({ fontAwesomeIcon, name, url, color }) => (
+  <Tippy content={name} placement="bottom" trigger="mouseenter">
+    <IconLink
+      href={url}
+      target="_blank"
+      color={color}
+      rel="noreferrer"
+      aria-label={name}
+    >
+      <FontAwesomeIcon name={fontAwesomeIcon} />
+    </IconLink>
+  </Tippy>
+);
+
+SocialLink.propTypes = {
+  fontAwesomeIcon: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+  color: PropTypes.string,
+};
+
+export default SocialLink;
