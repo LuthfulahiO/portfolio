8ef1a393174c794a516d16f9366e09848bf32d4a
Luthfulahi <luteeph@gmail.com>
2019-10-31 15:49:47 +0100

    fixed manifest config

--- manifest-config.js
+++ manifest-config.js
@@ -1,11 +1,11 @@
 const colors = require('./colors');
 
 module.exports = {
-  name: 'Mate Gatsby Starter',
-  short_name: 'Mate Gatsby Starter',
+  name: 'Luthfulahi Portfolio',
+  short_name: 'Luthfulahi',
   start_url: '/',
   background_color: colors.background,
   theme_color: colors.primary,
   display: 'minimal-ui',
-  icon: 'media/icon.png',
+  icon: 'src/components/Logo/Logo.png',
 };
